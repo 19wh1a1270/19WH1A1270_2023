f26b69eb9a6fd7ba1966be44ff78731687c78bed
19wh1a1270 <69143434+19wh1a1270@users.noreply.github.com>
2021-12-17 22:08:45 +0530

    Add files via upload

---
+++ LeetCode/Plus One/Plus One.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size(), i;
+        for(i = n - 1; i >= 0; i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]++; 
+                return digits;
+            } 
+            else
+                digits[i] = 0;
+        }
+        vector<int> ans(n + 1, 0);
+        ans[0] = 1;
+        return ans;
+    }
+};
